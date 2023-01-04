071f5a4400e603301b47e05042da9f7ad9061f80
Abbk1109 <abbk1983@gmail.com>
2023-01-04 08:18:14 -0800

    python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
 """
-
-This module contains a function that indents texts
-
+text identation module:
+indents text
+useful
 """
 
 
 def text_indentation(text):
-    '''This function prints a text with 2 new lines after each ".", "?", or ":"
-
-    Args:
-        text (str): The string to be printed
-
-    Raises:
-        TypeError: If text is not a string
-
-    '''
-
-    if not isinstance(text, str):
+    """text must be a string, There should be no space
+    at the beginning
+    or at the end of each printed line.
+    """
+    # text is a string, otherwise raise a TypeError
+    if isinstance(text, str) is False:
         raise TypeError("text must be a string")
 
-    count = 0
-    while count < len(text) and text[count] == " ":
-        count = count + 1
+    # if text is None or not isinstance(text, str) or len(text) < 0:
+    # raise TypeError('must be a string')
+    # string.replace(old, new, count)
+    # prints a text with 2 new lines
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    # 1) cut the entire line with \n
+    # 2) cut by separator with \n
+    # to the line that was strip (for.,?)
+    # loop text and split where "\n" is)
+    # end =""no space output promp line)
+    # there should be no spaces at the beginning
+    # and the end, line strip metodo
 
-    while count < len(text):
-        print(text[count], end="")
-        if text[count] == "\n" or text[count] in ".?:":
-            if text[count] in ".?:":
-                print("\n")
-            count = count + 1
-            while count < len(text) and text[count] == " ":
-                count = count + 1
-            continue
-        count = count + 1
+    print("\n".join([line.strip() for line in text.split("\n")]), end="")
+
+    # if isinstance(text, str) is False:
+    # raise TypeError("text must be a string")
+    # delim = ":.?"
+    # for x in delim:
+    # text = text.replace(x, "{}\n\n".format(x))
+    # text = text.replace('\\', '')
+    # text = "\n".join([line.strip() for line in text.split('\n')])
+    # print(text, end="")
